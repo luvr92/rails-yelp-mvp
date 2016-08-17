73a27525f94db984833a46eec48f308c709d51bc
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-17 12:19:13 +0100

    fixed validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
   CATEGORY = ["Chinese", "French", "Italian", "Indian", "Japanese", "Anglo-Saxon", "Middle-Eastern"]
   has_many :reviews
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :Restaurant
+  validates :restaurant_id, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: (1..5).to_a }
 end
